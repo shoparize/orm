5dce4cb4c9a31e576606be0711007332188b9b4b
Matthew Baggett <matthew@baggett.me>
2020-06-17 12:12:16 +0200

    Add some sensible defaults that are used repeatedly.

--- src/Migrations/Migration.php
+++ src/Migrations/Migration.php
@@ -7,4 +7,14 @@ use Phinx\Migration\AbstractMigration;
 abstract class Migration extends AbstractMigration
 {
 
+    protected array $defaultRelationshipOptions = [
+        'delete'=> 'NO_ACTION',
+        'update'=> 'NO_ACTION',
+    ];
+
+    protected array $enumYesNoOptions = [
+        'values' => ['Yes', 'No'],
+        'default' => 'No',
+    ];
+
 }
