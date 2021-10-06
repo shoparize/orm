703ccdc93e4af1381b0891117eefe8dd32e493a6
Alper <aradhell@users.noreply.github.com>
2021-10-06 17:24:58 +0200

    Update LaminatorSql.php

--- src/LaminatorSql.php
+++ src/LaminatorSql.php
@@ -4,7 +4,7 @@ namespace Benzine\ORM;
 
 use Laminas\Db\Adapter\AdapterInterface;
 
-class LaminatorSql extends \Zend\Db\Sql\Sql
+class LaminatorSql extends \Laminas\Db\Sql\Sql
 {
     public function __construct(AdapterInterface $adapter, $table = null, $sqlPlatform = null)
     {
